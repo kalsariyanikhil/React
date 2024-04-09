40fe97609ad0fbef01d93c59bbc30193f4532855
nikhil kalsariya <nickahir0786@gmail.com>
2024-04-09 10:18:45 +0530

    Topics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
-import Tag,{Tag2,Tag3,Tag4,Tag5} from './componeds/Tag.js'
+// import Tag,{Tag2,Tag3,Tag4,Tag5,Tag6,Tag7} from './componeds/Tag.js'
+import Count from './componeds/Count.js';
 
 function App() {
   return (
     <div className="App">
-      <Tag/>
+      {/* <Tag/>
       <Tag2/>
       <Tag3/>
       <Tag4/>
       <Tag5/>
+      <Tag6/>
+      <Tag7/>
+       */}
+      <Count/>
       </div>
       
   );
--- src/componeds/Tag.js
+++ src/componeds/Tag.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "../componeds/Styel.css"
 import Styel1 from "../componeds/Styel1.module.css" 
+import img from "../img/about-6.jpg"
 
 function Tag (){
     return <>
     <h1 style={{color:"black"}}>Jay Shrre Ram...</h1>
-    <h1 style={{color:"black"}}>Jay Shrre Ram...</h1>
-    <h1 style={{color:"black"}}>Jay Shrre Ram...</h1>
+    <h2 style={{color:"black"}}>Jay Shrre Ram...</h2>
+    <h3 style={{color:"black"}}>Jay Shrre Ram...</h3>
     </>
 }
 function Tag2(){
     return <>
+
     <h1 style={{color:"red"}}>Jay Murlidhar...</h1>
-    <h1 style={{color:"red"}}>Jay Murlidhar...</h1>
-    <h1 style={{color:"red"}}>Jay Murlidhar...</h1>
+    <h2 style={{color:"red"}}>Jay Murlidhar...</h2>
+    <h3 style={{color:"red"}}>Jay Murlidhar...</h3>
     </>
 }
 
@@ -32,13 +34,43 @@ function Tag4(){
 function Tag5(){
     return <ul>
             <li className={Styel1.abc}>Home</li>
-            <li>Data</li>
+            <li className="mt-5 text-center text-red-600">Data</li>
             <li>Contact</li>
             <li>Blog</li>
         </ul>
 }
 
+function Tag6(){
+    let style = {
+        color:"red"
+    }
+    return <h1 style={style}>Nikhil</h1>
+}
 
 
-export default Tag;
-export {Tag2,Tag3,Tag4,Tag5}
+    // const A = new Date(2024,4,5,13)
+    // let time = A.getHours();
+    // let Timing ="";
+
+    // if (time < 12) {
+    //     Timing = "GooD Morning"
+    // } else if (time >= 12 && time < 19) {
+    //     Timing = "GooD AfterNoon"
+    // }
+    // else{
+    //     Timing = "GooD Night"
+    // }
+
+    // function Tag7 (){
+    //     return <div>
+    //         <img src={img}/>
+    //         <h1 className="text-yellow-500 bg-black font-bold text-2xl">
+    //             Hello Mam -- {Timing}
+    //         </h1>
+    //     </div>
+    // }
+
+
+
+// export default Tag;
+// export {Tag2,Tag3,Tag4,Tag5,Tag6}
